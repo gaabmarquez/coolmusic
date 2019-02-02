0bb98b98c4d7da96c65cf779801fd5d7602d64ce
Gabriel Marquez <gaabmarquez@gmail.com>
2019-02-02 13:46:36 -0300

    Landing Page WIP

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { LandingComponent } from './components/landing/landing.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { LandingComponent } from "./components/landing/landing.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [NavigationComponent, LandingComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [NavigationComponent]
+  declarations: [NavigationComponent, LandingComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [NavigationComponent, FooterComponent]
 })
-export class CoreModule { }
+export class CoreModule {}
